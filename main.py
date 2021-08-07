6a9705a01d630fd7e69bc8bb506d9714174d2309
hamolicious <kickfliptv8@gmail.com>
2021-08-07 16:58:42 +0100

    added example cod

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from math import degrees, radians
-from random import randint
 import pygame
 from time import time
-from pygame import constants
 
 from vector.color import Color
 from vector.vector2d import Vec2d
-from vector.vector3d import Vec3d
 from ik import InverseKinematicsActor2D, Constraints, Simulation
 
 
@@ -21,10 +17,54 @@ clock, fps = pygame.time.Clock(), 0
 delta_time = 0 ; frame_start_time = 0
 #endregion
 
-ik = InverseKinematicsActor2D([300, 300], [50 for i in range(3)])
 Simulation.simulation_step = 0.0001
-Simulation.epsilon = 5
-ik.bones[-1].constraint = lambda x : Constraints.clamp(0, 180, x)
+Simulation.epsilon = 1
+
+class Spider:
+	def __init__(self) -> None:
+		self.body_rad = 25
+		self.pos = Vec2d(300)
+
+		self.legs = [
+			InverseKinematicsActor2D(self.pos, [50, 50, 50]),
+			InverseKinematicsActor2D(self.pos, [50, 50, 50])
+		]
+
+		self.__set_up_constraints()
+
+		dist = Vec2d(100, 50)
+		self.legs[0].set_target_pos(self.pos + dist)
+
+		dist.x *= -1
+		self.legs[1].set_target_pos(self.pos + dist)
+
+
+	def __set_up_constraints(self):
+		self.legs[0].bones[0].constraint = lambda x : Constraints.clamp(250, 350, x)
+		self.legs[0].bones[1].constraint = lambda x : Constraints.clamp(0, 180, x)
+
+		self.legs[1].bones[0].constraint = lambda x : Constraints.clamp(150, 300, x)
+		self.legs[1].bones[1].constraint = lambda x : Constraints.clamp(250, 360, x)
+
+	def __position_legs(self):
+		self.legs[0].pos = self.pos + Vec2d(self.body_rad, 0)
+		self.legs[1].pos = self.pos + Vec2d(-self.body_rad, 0)
+
+
+	def update(self):
+		self.__position_legs()
+
+		print(self.legs[0].pose_vector[0])
+
+		self.legs[0].update(30)
+		self.legs[1].update(30)
+
+	def display(self, screen):
+		self.legs[0].display_debug(screen)
+		self.legs[1].display_debug(screen)
+		pygame.draw.circle(screen, Color(255).get(), self.pos.get(), self.body_rad)
+
+spider = Spider()
 
 while True:
 	for event in pygame.event.get():
@@ -39,10 +79,10 @@ while True:
 	key_press   = pygame.key.get_pressed()
 
 	if mouse_press[0]:
-		ik.set_target_pos(mouse_pos)
+		spider.pos.set(mouse_pos)
 
-	ik.update()
-	ik.display(screen)
+	spider.update()
+	spider.display(screen)
 
 	pygame.display.update()
 	clock.tick(fps)
